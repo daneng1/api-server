5c86e2a0ff593348330bbc0dee25d88040642306
daneng1 <engeldb@gmail.com>
2021-04-22 21:05:02 -0700

    updated .env

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const PORT = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://localhost:27017/food';
+const MONGODB_URI = process.env.MONGODB_URI;
 
 const options = { useNewUrlParser: true, useUnifiedTopology: true };
 
