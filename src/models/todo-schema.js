aaa8a42d83370ffad13c5ed804f14e5f4dfb629a
daneng1 <engeldb@gmail.com>
2021-06-06 13:36:40 -0700

    updated todo schema

--- src/models/todo-schema.js
+++ src/models/todo-schema.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const todoSchema = mongoose.Schema({
-  completed: { type: Boolean, required:true},
+  completed: { type: String, required:true},
   text: { type: String, required:true},
   difficulty: { type: Number, required:true},
   assignee: { type: String, required:true},
