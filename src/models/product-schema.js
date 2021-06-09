3d6af091ad8c85f80ee515b549c9c37278f2dbcf
daneng1 <engeldb@gmail.com>
2021-06-09 14:21:35 -0700

    added product route, schema and server files

---
+++ src/models/product-schema.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const productSchema = mongoose.Schema({
+  category: {type: String, require:true, enum: ['patio', 'kitchen', 'games']},
+  name: { type:String, required:true},
+  price: { type: String, required:true},
+  image: {type: String, required:true},
+  inventory: { type:Number, required: true},
+});
+
+const productModel = mongoose.model('clothes', productSchema);
+
+module.exports = productModel;
