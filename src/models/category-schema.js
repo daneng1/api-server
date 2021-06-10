5bbdaa989c28a6aa3ed3ba98a2bb0a4e6fbdcbe4
daneng1 <engeldb@gmail.com>
2021-06-09 19:27:26 -0700

    added displayname and active to categories

--- src/models/category-schema.js
+++ src/models/category-schema.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const categorySchema = mongoose.Schema({
   name: { type:String, required:true},
   description: { type: String, required:true},
+  displayName: {type:String, required:true},
+  active: {type:Boolean, required:true},
 });
 
 const categoryModel = mongoose.model('category', categorySchema);
